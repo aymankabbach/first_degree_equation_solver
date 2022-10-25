12e62d289b42fb1ac0eb5364521dc79356a8fc84
ayman <kabbachayman95@gmail.com>
2022-10-25 22:47:22 +0100

    small modification

--- first_equation_solver.cpp
+++ first_equation_solver.cpp
@@ -8,20 +8,20 @@ float read_user_input_value(string variable)
     cin>>float_number;
     return float_number;
 }
-bool check_speciale_case(float variable_a,float variable_b)
+bool check_speciale_case(float &variable_a)
 {
     return (variable_a==0);
 }
-float solve_equation(float variable_a,float variable_b)
+float solve_equation(float &variable_a,float &variable_b)
 {
     float result=-variable_b/variable_a;
     return result;
 }
-void print_special_case_result(float variable_a,float variable_b)
+void print_special_case_result(float &variable_a,float &variable_b)
 {
     (variable_b==0)?cout<< "all numbers are solutions to this equation"<<endl:cout<<"this equation has no solution"<<endl;
 }
-void print_result(float variable_a,float variable_b)
+void print_result(float &variable_a,float &variable_b)
 {
     float result=solve_equation(variable_a,variable_b);
     cout<<"result = "<<result<<endl;
@@ -30,6 +30,8 @@ int main()
 {
     float variable_a=read_user_input_value("a");
     float variable_b=read_user_input_value("b");
-    bool special_case=check_speciale_case(variable_a,variable_b);
+    bool special_case=check_speciale_case(variable_a);
     (special_case) ? print_special_case_result(variable_a,variable_b):print_result(variable_a,variable_b);
+    system("pause");
+    return 0;
 }
