93b0b8f296222127fc67bdf008df603041f28294
ayman <aymankabbach95@gmail.com>
2023-05-04 23:23:14 +0200

    first degree equation solver c# version

---
+++ 1st_degree_equation_solver/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace _1st_degree_equation_solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textX = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Degree Equation Solver";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "enter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "a";
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(53, 152);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(134, 20);
+            this.textA.TabIndex = 3;
+            this.textA.Validating += new System.ComponentModel.CancelEventHandler(this.textA_Validating);
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(289, 152);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(134, 20);
+            this.textB.TabIndex = 5;
+            this.textB.Validating += new System.ComponentModel.CancelEventHandler(this.textB_Validating);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(248, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "b";
+            // 
+            // textX
+            // 
+            this.textX.Location = new System.Drawing.Point(55, 234);
+            this.textX.Name = "textX";
+            this.textX.Size = new System.Drawing.Size(134, 20);
+            this.textX.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "x = ";
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(518, 152);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(75, 23);
+            this.btnSolve.TabIndex = 8;
+            this.btnSolve.Text = "solve";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(12, 21);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 9;
+            this.BtnReset.Text = "reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.textX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textX;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ 1st_degree_equation_solver/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1st_degree_equation_solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool textAorBisEmpty()
+        {
+            if (textA.Text=="")
+            {
+                return true;
+            }
+            else if (textB.Text=="")
+            { return true;}
+            else { return false; }
+        }
+        private bool is_textAnumbers()
+        {
+            try
+            {
+                double Var_A = Convert.ToDouble(textA.Text);
+                return true;
+            }
+            catch { return false; }
+        }
+        private bool is_textBnumbers()
+        {
+            try
+            {
+                double Var_B = Convert.ToDouble(textB.Text);
+                return true;
+            }
+            catch { return false; }
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            if (textAorBisEmpty()==false)
+            {
+                try
+                {
+                    double Var_A = Convert.ToDouble(textA.Text);
+                    double Var_B = Convert.ToDouble(textB.Text);
+                    if (Var_A == 0 && Var_B == 0)
+                    {
+                        MessageBox.Show("All numbers are solutions to this Equation");
+                    }
+                    else if (Var_A == 0)
+                    {
+                        MessageBox.Show("there is no solution to this equation");
+                    }
+                    else
+                    {
+                        string result = String.Format("{0:0.00}", (-Var_B / Var_A));
+                        textX.Text = result;
+                    }
+                }
+                catch { }
+                
+            }
+        }
+
+        private void textA_Validating(object sender, CancelEventArgs e)
+        {
+            if (is_textAnumbers()==false)
+            {
+                e.Cancel=true;
+                textA.Focus();
+                errorProvider1.SetError(textA, "please enter a number");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(textA, "");
+            }
+        }
+
+        private void textB_Validating(object sender, CancelEventArgs e)
+        {
+            if (is_textBnumbers() == false)
+            {
+                e.Cancel = true;
+                textB.Focus();
+                errorProvider1.SetError(textB, "please enter a number");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(textB, "");
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            textA.Text = "";
+            textB.Text = "";
+            textX.Text = "";
+        }
+    }
+}
